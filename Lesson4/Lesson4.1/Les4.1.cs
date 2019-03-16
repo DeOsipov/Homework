cb86c5706923e0f627dad0a56bfc1f9f96bc260f
Denis Osipov <whomi@mail.ru>
2019-03-16 23:09:14 +0300

    no matter

--- Lesson4/Lesson4.1/Les4.1.cs
+++ Lesson4/Lesson4.1/Les4.1.cs
@@ -7,31 +7,52 @@ using System.IO;
 
 namespace Lesson4._1
 {
-    //1. Получить всё дерево папок и файлов входящих в эти папки (вложенность папок должна быть не меньше 3х папок и не меньше 4х файлов),
-    //запрещено использовать массивы. Можете для примера потренироваться на папке Program Files или C:\Users\%USERNAME%\.
+
     class Program
     {
-        static SubDirectoryName(string path)
-        {
-            string[] dir = Directory.GetDirectories(path);
-
-            for (int i = 0; i < dir.Length; i++)
-            {
-                Console.WriteLine(dir[i]);
-            }
-        }
-
-        static string SubDirectoryNameLoop(string path)
-        {
-            return SubDirectoryName(path);
-        }
+        static int level = 0;
 
         static void Main(string[] args)
         {
-            string path = @"C:/Program Files";
+            string path = @"C:\Program files";
 
-            SubDirectoryName(path);
-            Console.ReadLine();
+            string[] dir = Directory.GetDirectories(path);
+
+            for (int position = 0; position < dir.Length || dir.Length == 0; position++)
+            {
+                if (dir.Length == 0)
+                {
+                    level--;
+                    if (level < 0)
+                        break;
+                }
+                else
+                {
+                    Console.WriteLine(dir[position]);
+                };
+
+                Loop(dir, position);
+                Console.Read();
+            }
+        }
+
+        static void Loop(string[] dir, int position)
+        {
+            string[] subDir = Directory.GetDirectories(dir[position]);
+
+            for (int i = 0; i < dir.Length || dir.Length == 0; i++)
+            {
+                if (subDir.Length == 0)
+                {
+                    level--;
+                    if (level < 0)
+                        break;
+                }
+                else
+                {
+                    Console.WriteLine(subDir[i]);
+                }
+            }
         }
     }
 }
